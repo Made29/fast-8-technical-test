95796baada9dea7c0cabc661590792a998ba966f
MadeJWK <96839176+Made29@users.noreply.github.com>
2022-06-08 01:55:48 +0700

    set up redux finish

---
+++ gadjian/src/store/reducers/reducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+    data: []
+}
+
+function dataReducer(state = initialState, action) {
+    switch (action.type) {
+      case 'counter/incremented':
+        return { value: state.value + 1 }
+      case 'counter/decremented':
+        return { value: state.value - 1 }
+      default:
+        return state
+    }
+  }
+
+export default dataReducer;
